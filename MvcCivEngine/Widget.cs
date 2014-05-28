2500c13c4e33e9d76001fb91d990c48f017fc481
Tim Serverius <tserverius@alegeus.com>
2014-05-28 15:27:46 -0400

    widget added

---
+++ MvcCivEngine/Widget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcCivEngine
+{
+    public class Widget
+    {
+        public string Name { get; set; }
+    }
+}
